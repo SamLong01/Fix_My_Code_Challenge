a16b718fcd854db40d349e3dbd629afa8c7a3818
SamLong01 <samueladebayo1015@gmail.com>
2023-02-22 15:10:56 +0100

    next

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,53 +1,44 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - Delete a node at a specific index from a list
+ * delete_dnodeint_at_index - deletes the node at index of a dlistint_t linked list
+ * @head: double pointer to the head of the list
+ * @index: index of the node to be deleted, starting from 0
  *
- * @head: A pointer to the first element of a list
- * @index: The index of the node to delete
- *
- * Return: 1 on success, -1 on failure
+ * Return: 1 if successful, -1 if failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *saved_head;
-	dlistint_t *tmp;
-	unsigned int p;
+    dlistint_t *current_node = *head;
+    dlistint_t *node_to_delete;
 
-	if (*head == NULL)
-	{
-		return (-1);
-	}
-	saved_head = *head;
-	p = 0;
-	while (p < index && *head != NULL)
-	{
-		*head = (*head)->next;
-		p++;
-	}
-	if (p != index)
-	{
-		*head = saved_head;
-		return (-1);
-	}
-	if (0 == index)
-	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
-		if (tmp != NULL)
-		{
-			tmp->prev = NULL;
-		}
-	}
-	else
-	{
-		(*head)->prev->prev = (*head)->prev;
-		free(*head);
-		if ((*head)->next)
-			(*head)->next->prev = (*head)->prev;
-		*head = saved_head;
-	}
-	return (1);
+    if (head == NULL || *head == NULL)
+        return (-1);
+
+    if (index == 0)
+    {
+        *head = current_node->next;
+        if (*head != NULL)
+            (*head)->prev = NULL;
+        free(current_node);
+        return (1);
+    }
+
+    for (unsigned int i = 0; i < index - 1; i++)
+    {
+        current_node = current_node->next;
+        if (current_node == NULL)
+            return (-1);
+    }
+
+    node_to_delete = current_node->next;
+    if (node_to_delete == NULL)
+        return (-1);
+
+    current_node->next = node_to_delete->next;
+    if (node_to_delete->next != NULL)
+        node_to_delete->next->prev = current_node;
+
+    free(node_to_delete);
+    return (1);
 }
